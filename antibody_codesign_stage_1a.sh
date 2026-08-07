727922b8d4c5f0cfd58927a221eeea30b6a9ed7d
eding36 <eding3602@gmail.com>
2026-08-06 22:42:02 -0700

    clean

--- antibody_codesign_stage_1a.sh
+++ antibody_codesign_stage_1a.sh
@@ -37,7 +37,7 @@ torchrun --standalone --nproc_per_node=2 /home/dinge/Protenix/runner/train.py \
 --project protenix \
 --use_wandb true \
 --diffusion_batch_size 1 \
---eval_interval 10000 \
+--eval_interval 5000 \
 --eval_ema_only true \
 --test_max_n_token 1024 \
 --log_interval 50 \
